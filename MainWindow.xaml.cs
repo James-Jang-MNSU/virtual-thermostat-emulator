1e83f413c1c596522af1a4d42de2361a7c728d89
James Jang <james.jang@mnsu.edu>
2025-12-18 13:00:37 -0600

    Initial commit of Thermostat HIL Simulation

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.IO;
+
+namespace ThermostatEmulator
+{
+    public partial class MainWindow : Window
+    {
+        // class variables
+        private Thermostat thermostat;
+        private DispatcherTimer? simulationTimer; // simulates the time flow
+
+        // constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // initialize thermostat with (anInitialTemp: 20, aTargetTemp: 22)
+            thermostat = new Thermostat(20.0, 22.0);
+
+            // setup the display immediately
+            UpdateDisplay();
+        }
+
+        // methods
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (simulationTimer == null) // timer has not been created yet, or has been destroyed... the simulation is OFF
+            {
+                
+                simulationTimer = new DispatcherTimer(); // create the timer
+                simulationTimer.Interval = TimeSpan.FromSeconds(0.1); // set speed (every 0.1 seconds)
+                simulationTimer.Tick += SimulationTick; // connect the method to run
+                simulationTimer.Start();
+                btnStart.Content = "POWER OFF";
+            }
+            else
+            {
+                // stop logic
+                simulationTimer.Stop();
+                simulationTimer = null; // delete the timer object
+                btnStart.Content = "POWER ON";
+            }
+        }
+        private void SimulationTick(object sender, EventArgs e)
+        {
+            thermostat.RunControlCycle(); // run the physics/logic 
+            UpdateDisplay();
+            
+            // broadcasting data
+            try
+            {
+                // try to write the current temperature to the file named "status.txt"
+                string statusData = thermostat.CurrentTemperature.ToString("F2");
+                File.WriteAllText("status.txt", statusData);
+            }
+            catch (IOException)
+            {
+                // If we catch an IOException (File is busy), 
+                // we just do nothing and try again next tick.
+                // This prevents the crash.
+            }
+        }
+        private void UpdateDisplay()
+        {
+            // update text
+            txtCurrentTemp.Text = thermostat.CurrentTemperature.ToString("F1") + "°";
+            txtTargetTemp.Text = thermostat.TargetTemperature.ToString("F0") + "°";
+
+            // check if simulation is running
+            if (simulationTimer == null || !simulationTimer.IsEnabled)
+            {
+                // if stopped, show "OFF" in Grey
+                txtStatus.Text = "OFF";
+                txtStatus.Foreground = Brushes.Gray;
+                txtTargetTemp.Foreground = Brushes.Gray;
+                return; // Stop here! Don't run the rest of the logic.
+            }
+
+            // if running, show the actual status colors
+            if (thermostat.IsHeating)
+            {
+                txtStatus.Text = "HEATING";
+                txtStatus.Foreground = Brushes.OrangeRed;
+                txtTargetTemp.Foreground = Brushes.OrangeRed;
+            }
+            else
+            {
+                txtStatus.Text = "ECO";
+                txtStatus.Foreground = Brushes.DeepSkyBlue;
+                txtTargetTemp.Foreground = Brushes.White;
+            }
+        }
+        private void btnWarmer_Click(object sender, RoutedEventArgs e)
+        {
+            thermostat.TargetTemperature += 1.0;
+            UpdateDisplay();
+        }
+        private void btnCooler_Click(object sender, RoutedEventArgs e)
+        {
+            thermostat.TargetTemperature -= 1.0;
+            UpdateDisplay();
+        }
+    }
+}
---
+++ Thermostat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThermostatEmulator
+{
+    public class Thermostat
+    {
+        // class variables
+        private double currentTemperature;
+        private double targetTemperature;
+        private bool isHeating;
+
+        private const double Deadband = 1.0; // 1.0 degree Celsius buffer
+        private readonly Random _random = new Random(); // used for simulating temperature drift
+
+        // gets and sets
+        public double CurrentTemperature
+        {
+            get { return this.currentTemperature; }
+            private set { this.currentTemperature = value; }
+        }
+        public double TargetTemperature
+        {
+            get { return this.targetTemperature; }
+            set { this.targetTemperature = value; }
+        }
+        public bool IsHeating
+        {
+            get { return this.isHeating; }
+            private set { this.isHeating = value; }
+        }
+
+        // constructors
+        public Thermostat(double anInitialTemp, double aTargetTemp)
+        {
+            this.currentTemperature = anInitialTemp;
+            this.targetTemperature = aTargetTemp;
+
+            UpdateHVACState(); // automatically set's the hvac state
+        }
+        
+        // class methods
+        public void ReadCurrentTemp()
+        {
+            double randomChange = (_random.NextDouble()* 2.0 - 1.0) * 0.05; // a random number between -0.05 and +0.05, used to simulate the noise in temperature readings
+            double hvacInfluence = 0.0; // the amount of influence the current hvac state has on the temperature
+
+            if (this.IsHeating)
+            {
+                hvacInfluence = 0.2; // when heating is ON, the hvac influence acts as positive bias
+            }
+            else
+            {
+                hvacInfluence = -0.1; // when heating is OFF, the hvac influence acts as negative bias
+            }
+            this.CurrentTemperature += randomChange + hvacInfluence;
+        }
+        public void UpdateHVACState()
+        {
+            if (!this.IsHeating && this.CurrentTemperature < this.TargetTemperature - Thermostat.Deadband)
+            {
+                IsHeating = true;
+            }
+            else if (this.IsHeating && this.CurrentTemperature >= this.TargetTemperature)
+            {
+                IsHeating = false;
+            }
+        }
+        public void RunControlCycle()
+        {
+            this.ReadCurrentTemp();
+            this.UpdateHVACState();
+        }
+    }
+}
